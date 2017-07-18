1a66202d31814dfa42b3a3b8b2c7107f17976591
Edmundas Riskus <edmundas@riskus.lt>
2017-07-19 00:07:36 +0300

    Form integration + Country list + Search POST handler

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var countriesList = require('countries-list');
+var CONTINENTS = countriesList.continents;
+var COUNTRIES = countriesList.countries;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', continents: CONTINENTS, countries: COUNTRIES });
 });
+
+/* POSt search. */
+router.post('/search', function(req, res, next) {
+  var data = req.body;
+  res.json({});
+});
+
 
 module.exports = router;
